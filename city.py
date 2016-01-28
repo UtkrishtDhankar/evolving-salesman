f834d216cb09af26a3f45a1a154092a9464d2e9e
Utkrisht Dhankar <utkrishtdhankar@gmail.com>
2016-01-28 18:32:08 +0530

    Added total_distance()

--- city.py
+++ city.py
@@ -1,5 +1,13 @@
 from math import hypot
 
+def total_distance(route):
+  """Calculates the total distance taken by the route"""
+  d = 0
+  for i in range(len(route) - 1):
+    d += distance(route[i], route[i + 1])
+
+  return d
+
 def distance(a, b):
   """Returns the distance between two cities, a and b"""
   return hypot(a.x - b.x, a.y - b.y)
